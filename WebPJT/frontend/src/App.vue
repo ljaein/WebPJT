89af2ac7f8d14eb6926740ef4924165f16819c25
이재인 <skyfin59@naver.com>
2020-07-21 14:12:23 +0900

    토큰 생성

---
+++ WebPJT/frontend/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <Header :isHeader="isHeader"/>
+    <LoginModal/>
+    <router-view/>
+  </div>
+</template>
+
+<script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>  
+<script> 
+import './assets/css/style.scss' 
+import Header from './components/common/Header.vue'
+import LoginModal from './components/modal/LoginModal.vue'
+import constants from './lib/constants' 
+
+export default {
+  name: 'App',
+  components: { 
+    Header,
+    LoginModal,
+  },
+  created() {
+      let url = this.$route.name;
+
+      this.checkUrl(url);
+  },
+  watch: {
+      $route (to){
+
+          this.checkUrl(to.name);
+      }
+  },
+  methods : {
+      checkUrl(url) { 
+
+          let array = [
+              constants.URL_TYPE.USER.LOGIN,
+
+          ];
+
+          let isHeader = true;
+          array.map(path => {
+              if (url === path)
+                  isHeader = false;
+          })
+          this.isHeader = isHeader;
+
+      },
+  },
+  data: function () {
+        return {
+            isHeader: true,
+            constants
+        } 
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; 
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
